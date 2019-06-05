f792dd81e30a4261b99eb39134050e79a98a2ede
haiyanglx <524788971@qq.com>
2019-06-05 19:57:34 +0800

    Update vtest_BufferManager.h

--- common/inc/vtest_BufferManager.h
+++ common/inc/vtest_BufferManager.h
@@ -1,7 +1,4 @@
-/*-------------------------------------------------------------------
-Copyright (c) 2013-2014 Qualcomm Technologies, Inc. All Rights Reserved.
-Qualcomm Technologies Proprietary and Confidential
---------------------------------------------------------------------*/
+
 
 #ifndef _VTEST_BUFFER_MANAGER_H
 #define _VTEST_BUFFER_MANAGER_H
