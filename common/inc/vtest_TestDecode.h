b10bb8be3c6b5b2fb7a2a6b757f9717acb2c26a1
haiyanglx <524788971@qq.com>
2019-06-05 20:25:04 +0800

    Update vtest_TestDecode.h

--- common/inc/vtest_TestDecode.h
+++ common/inc/vtest_TestDecode.h
@@ -1,7 +1,4 @@
-/*-------------------------------------------------------------------
-Copyright (c) 2013-2014 Qualcomm Technologies, Inc. All Rights Reserved.
-Qualcomm Technologies Proprietary and Confidential
---------------------------------------------------------------------*/
+
 
 #ifndef _VTEST_TEST_DECODE_H
 #define _VTEST_TEST_DECODE_H
